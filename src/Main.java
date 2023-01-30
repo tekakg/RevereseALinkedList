0a30b84df54c5c5086e55d4e9ff55b96d75f48a0
Aditya <akgupta@tekion.com>
2023-01-31 00:03:51 +0530

    Second Method

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,21 @@ public class Main {
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<>();
         // Taking 5 integers as input.
-        for (int i = 0; i < 51; i++) {
+        int siz=5;
+        for (int i = 0; i < siz; i++) {
             Scanner src = new Scanner(System.in);
             int val = src.nextInt();
             list.add(val);
         }
         //After formatting
         System.out.println(list);
-        Collections.reverse(list);
+        for(int i=0;i<siz/2;i++)
+        {
+            int startElement=list.get(i);
+            int lastElement=list.get(siz-i-1);
+            list.set(siz-i-1,startElement);
+            list.set(i,lastElement);
+        }
         System.out.println(list);
     }
 }
