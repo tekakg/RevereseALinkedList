60a9de41d59cfdcbea6849e3a9ba69aacf6a3d63
Aditya <akgupta@tekion.com>
2023-01-30 23:30:22 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer>list=new LinkedList<>();
+        // Taking 5 integers as input.
+        for(int i=0;i<51;i++)
+        {
+            Scanner src=new Scanner(System.in);
+            int val=src.nextInt();
+            list.add(val);
+        }
+        System.out.println(list);
+        Collections.reverse(list);
+        System.out.println(list);
+    }
+}
